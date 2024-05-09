1d1c2aedb0120a806bf56900a11fe32e704965f7
RuloPonce04 <raulponcelopez2002@gmail.com>
2024-05-08 23:07:35 -0600

    Nuevos espacios y se agregaron constancias

--- sobreMi/sobreMi.js
+++ sobreMi/sobreMi.js
@@ -13,28 +13,31 @@ document.getElementById("btnPersonal").addEventListener("click", function() {
     }, 500); // La duración de la animación en milisegundos
 });
 
-document.getElementById("btnSkills").addEventListener("click", function() {
-    // Calcula la posición del elemento al que quieres desplazarte
-    var sectionSkills = document.getElementById("backgroundCoreSkills");
-    var sectionSkillsPosition = sectionSkills.offsetTop;
+// Obtener todos los botones de constancia
+const constancyButtons = document.querySelectorAll('.btnShowConstancy');
 
-    // Desplaza la ventana hacia la posición de la sección de habilidades de forma suave
-    sectionSkills.scrollIntoView({ behavior: 'smooth' });
-
-    // Agrega la clase de resaltar a la sección de habilidades al hacer clic en el botón Skills
-    sectionSkills.classList.add("resaltar");
-
-
+// Agregar evento clic a cada botón
+constancyButtons.forEach(button => {
+    button.addEventListener('click', function() {
+        // Obtener la superposición más cercana al botón clickeado
+        const overlay = this.parentElement.nextElementSibling;
+        overlay.style.display = 'flex';
+    });
 });
 
-document.getElementById("btnKnowledge").addEventListener("click", function() {
-    // Calcula la posición del elemento al que quieres desplazarte
-    var sectionSkills = document.getElementById("backgroundCoreFormation");
-    var sectionSkillsPosition = sectionSkills.offsetTop;
+// Agregar evento clic para cerrar la superposición
+document.querySelectorAll('.close').forEach(closeButton => {
+    closeButton.addEventListener('click', function() {
+        const overlay = this.parentElement.parentElement;
+        overlay.style.display = 'none';
+    });
+});
 
-    // Desplaza la ventana hacia la posición de la sección de habilidades de forma suave
-    sectionSkills.scrollIntoView({ behavior: 'smooth' });
-
-    // Agrega la clase de resaltar a la sección de habilidades al hacer clic en el botón Skills
-    sectionSkills.classList.add("resaltar");
+// Agregar evento clic para cerrar la superposición haciendo clic fuera de ella
+document.querySelectorAll('.overlay').forEach(overlay => {
+    overlay.addEventListener('click', function(event) {
+        if (event.target === this) {
+            this.style.display = 'none';
+        }
+    });
 });
