7edb745dbaeb2e73d193dfacb4729b88cf4d88f2
RuloPonce04 <raulponcelopez2002@gmail.com>
2024-05-08 23:33:32 -0600

    Arreglo botones

--- sobreMi/sobreMi.js
+++ sobreMi/sobreMi.js
@@ -13,6 +13,32 @@ document.getElementById("btnPersonal").addEventListener("click", function() {
     }, 500); // La duración de la animación en milisegundos
 });
 
+document.getElementById("btnSkills").addEventListener("click", function() {
+    // Calcula la posición del elemento al que quieres desplazarte
+    var sectionSkills = document.getElementById("backgroundCoreSkills");
+    var sectionSkillsPosition = sectionSkills.offsetTop;
+
+    // Desplaza la ventana hacia la posición de la sección de habilidades de forma suave
+    sectionSkills.scrollIntoView({ behavior: 'smooth' });
+
+    // Agrega la clase de resaltar a la sección de habilidades al hacer clic en el botón Skills
+    sectionSkills.classList.add("resaltar");
+
+
+});
+
+document.getElementById("btnKnowledge").addEventListener("click", function() {
+    // Calcula la posición del elemento al que quieres desplazarte
+    var sectionSkills = document.getElementById("backgroundCoreFormation");
+    var sectionSkillsPosition = sectionSkills.offsetTop;
+
+    // Desplaza la ventana hacia la posición de la sección de habilidades de forma suave
+    sectionSkills.scrollIntoView({ behavior: 'smooth' });
+
+    // Agrega la clase de resaltar a la sección de habilidades al hacer clic en el botón Skills
+    sectionSkills.classList.add("resaltar");
+});
+
 // Obtener todos los botones de constancia
 const constancyButtons = document.querySelectorAll('.btnShowConstancy');
 
